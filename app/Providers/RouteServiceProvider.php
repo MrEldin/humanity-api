8cefe4294524ea48669df5b1a6b5dd0bc541fe87
Eldin Mujovic <eldinpc@live.com>
2018-10-16 13:37:30 +0200

    fix(routes): Fix routes.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -37,8 +37,6 @@ class RouteServiceProvider extends ServiceProvider
     {
         $this->mapApiRoutes();
 
-        $this->mapWebRoutes();
-
         $this->mapUsersRoutes();
 
         $this->mapPhasesRoutes();
